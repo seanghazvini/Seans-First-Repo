21a2e66e42a9a8769e931acd41eb39813cc734e0
Sean Ghazvini <seanghazvini@gmail.com>
2018-06-01 16:30:41 -0700

    Able to store the name and code inside localStorage

--- App.js
+++ App.js
@@ -2,15 +2,27 @@ $(document).ready(function() {
  
  $('.setData').on('click', function() {
     let textFieldValue = $('.textField').val();
-    $('.debug').text(textFieldValue);
-  
-   localStorage.setItem('myFormTextData', textFieldValue);
+    //$('.debug').text(textFieldValue);
+
+    let textAreaValue = $('.textArea').val();
+    //$('.debug').text(textAreaValue);
+
+    localStorage.setItem('myFormTextData', textFieldValue);
+   $('.textField').val('');
+
+   localStorage.setItem('myFormTextArea', textAreaValue);
+   $('.textArea').val('');
 
 });
 
  $('.getData').on('click', function() {
    let retrievedData = localStorage.getItem('myFormTextData');
-   $('.debug').text(retrievedData);
+  // $('.debug').text(retrievedData);
+
+   let retrievedTextAreaData = localStorage.getItem('myFormTextArea');
+   //$('.debug').text(retrievedTextAreaData);
+
+   $('.textArea').text(retrievedTextAreaData);
  });
 
  // $('.textField').on('keyup', function() {
--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Goals:
 - [x] click handler to capture data in text form field
 - [x] helper utilities to read and write to local storage
 
-- [ ] Make it handle code! (use a textarea instead of a text)
-- [ ] Have two fields, one is a short name the other is the snippet
-- [ ] going to store the name and code inside localStorage
-- [ ] display all snippets
-- [ ] search for a snippet using the name
+- [x] Make it handle code! (use a textarea instead of a text)
+- [x] Have two fields, one is a short name the other is the snippet
+- [x] Going to store the name and code inside localStorage
+- [ ] Display all snippets
+- [ ] Search for a snippet using the name
