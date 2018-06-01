a9bd7f9ce395a3bbc1d6254efbe59302528cd1ec
Sean Ghazvini <seanghazvini@gmail.com>
2018-06-01 10:36:48 -0700

    Added jquery failover and localStorage hooks

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-  &('.submitForm').on('click', function() {
-    let textField = $('.textField').val();
-    $('.debug').text(textFieldValue)
-  });
+ 
+ $('.setData').on('click', function() {
+    let textFieldValue = $('.textField').val();
+    $('.debug').text(textFieldValue);
+  
+   localStorage.setItem('myFormTextData', textFieldValue);
+
 });
+
+ $('.getData').on('click', function() {
+   let retrievedData = localStorage.getItem('myFormTextData');
+   $('.debug').text(retrievedData);
+ });
+
+ // $('.textField').on('keyup', function() {
+ //    let textField = $('.textField').val();
+ //    $('.debug').text(textFieldValue);
+ //  });
+
+});
+
